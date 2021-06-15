283f98d6e60dc974cb24fae398f311427b00b3d7
PowerMetalPriest <official-mail.sheo@yandex.ru>
2021-06-15 13:52:32 +0300

    Добавлен 2.sql

---
+++ 2.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 15 2021 г., 13:49
+-- Версия сервера: 5.7.29
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test_samson`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_category`
+--
+
+CREATE TABLE `a_category` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `c_code` int(15) UNSIGNED NOT NULL,
+  `c_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_price`
+--
+
+CREATE TABLE `a_price` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_product`
+--
+
+CREATE TABLE `a_product` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `code` int(10) UNSIGNED NOT NULL,
+  `product_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_property`
+--
+
+CREATE TABLE `a_property` (
+  `product` varchar(50) NOT NULL,
+  `property` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `a_price`
+--
+ALTER TABLE `a_price`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `a_product`
+--
+ALTER TABLE `a_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `a_price`
+--
+ALTER TABLE `a_price`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `a_product`
+--
+ALTER TABLE `a_product`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  ADD CONSTRAINT `a_category_ibfk_1` FOREIGN KEY (`id`) REFERENCES `a_product` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `a_product`
+--
+ALTER TABLE `a_product`
+  ADD CONSTRAINT `a_product_ibfk_1` FOREIGN KEY (`id`) REFERENCES `a_price` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
