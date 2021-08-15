9638c21c08201c8e9dc9d0ad10f0e8778cedcbb6
PowerMetalPriest <official-mail.sheo@yandex.ru>
2021-08-15 13:49:30 +0300

    Update 2.sql

--- 2.sql
+++ 2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 16 2021 г., 14:16
+-- Время создания: Авг 15 2021 г., 13:47
 -- Версия сервера: 5.7.29
 -- Версия PHP: 7.1.33
 
@@ -28,10 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `a_category` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `c_code` int(15) UNSIGNED NOT NULL,
+  `id_category` int(10) UNSIGNED NOT NULL,
+  `code` int(10) UNSIGNED NOT NULL,
   `c_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `a_category`
+--
+
+INSERT INTO `a_category` (`id_category`, `code`, `c_name`) VALUES
+(31, 201, 'Бумага'),
+(32, 202, 'Бумага'),
+(33, 302, 'Принтеры'),
+(34, 305, 'Принтеры');
 
 -- --------------------------------------------------------
 
@@ -40,10 +50,24 @@ CREATE TABLE `a_category` (
 --
 
 CREATE TABLE `a_price` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `type` varchar(10) NOT NULL,
+  `code` int(10) UNSIGNED NOT NULL,
+  `type` varchar(15) NOT NULL,
   `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `a_price`
+--
+
+INSERT INTO `a_price` (`code`, `type`, `price`) VALUES
+(201, 'Базовая', 11.5),
+(201, 'Москва', 12.5),
+(202, 'Базовая', 18.5),
+(202, 'Москва', 22.5),
+(302, 'Базовая', 3010),
+(302, 'Москва', 3500),
+(305, 'Базовая', 3310),
+(305, 'Москва', 2999);
 
 -- --------------------------------------------------------
 
@@ -57,6 +81,16 @@ CREATE TABLE `a_product` (
   `product_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `a_product`
+--
+
+INSERT INTO `a_product` (`id`, `code`, `product_name`) VALUES
+(13, 201, 'Бумага А4'),
+(14, 202, 'Бумага А3'),
+(15, 302, 'Принтер Canon'),
+(16, 305, 'Принтер HP');
+
 -- --------------------------------------------------------
 
 --
@@ -64,9 +98,19 @@ CREATE TABLE `a_product` (
 --
 
 CREATE TABLE `a_property` (
-  `product` int(10) UNSIGNED NOT NULL,
+  `code` int(10) UNSIGNED NOT NULL,
   `property` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `a_property`
+--
+
+INSERT INTO `a_property` (`code`, `property`) VALUES
+(201, 'Плотность 100'),
+(202, 'Плотность 90'),
+(302, 'Формат A4'),
+(305, 'Формат A3');
 
 --
 -- Индексы сохранённых таблиц
@@ -76,36 +120,37 @@ CREATE TABLE `a_property` (
 -- Индексы таблицы `a_category`
 --
 ALTER TABLE `a_category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `a_price`
---
-ALTER TABLE `a_price`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_category`),
+  ADD KEY `code` (`code`);
 
 --
 -- Индексы таблицы `a_product`
 --
 ALTER TABLE `a_product`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `code` (`code`);
+  ADD UNIQUE KEY `code` (`code`);
 
 --
 -- Индексы таблицы `a_property`
 --
 ALTER TABLE `a_property`
-  ADD PRIMARY KEY (`product`);
+  ADD PRIMARY KEY (`code`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
 --
+-- AUTO_INCREMENT для таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  MODIFY `id_category` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
 -- AUTO_INCREMENT для таблицы `a_product`
 --
 ALTER TABLE `a_product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -115,14 +160,13 @@ ALTER TABLE `a_product`
 -- Ограничения внешнего ключа таблицы `a_category`
 --
 ALTER TABLE `a_category`
-  ADD CONSTRAINT `a_category_ibfk_1` FOREIGN KEY (`id`) REFERENCES `a_product` (`code`);
+  ADD CONSTRAINT `a_category_ibfk_1` FOREIGN KEY (`code`) REFERENCES `a_product` (`code`);
 
 --
--- Ограничения внешнего ключа таблицы `a_product`
+-- Ограничения внешнего ключа таблицы `a_property`
 --
-ALTER TABLE `a_product`
-  ADD CONSTRAINT `a_product_ibfk_1` FOREIGN KEY (`code`) REFERENCES `a_price` (`id`),
-  ADD CONSTRAINT `a_product_ibfk_2` FOREIGN KEY (`code`) REFERENCES `a_property` (`product`);
+ALTER TABLE `a_property`
+  ADD CONSTRAINT `a_property_ibfk_1` FOREIGN KEY (`code`) REFERENCES `a_product` (`code`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
